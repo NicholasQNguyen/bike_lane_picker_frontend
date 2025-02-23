0797ed696e7a1a58f282cd06de98e068a776ecf7
Nicholas Nguyen <nicholasqnguyen8@gmail.com>
2025-02-22 19:22:48 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyMap from "./components/MyMap/MyMap.tsx";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: '1000px', height: '1000px'}}>
+        <MyMap/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyMap/MyMap.tsx
@@ -0,0 +1,59 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { fromLonLat } from 'ol/proj';
+import 'ol/ol.css';
+
+const MyMap = () => {
+  // ol.proj.transform([-8367174.179297738, 4859025.894138464],"WGS84", "EPSG:900913");
+  const mapRef = useRef();
+  const [center, setCenter] = useState([-75.165, 39.952]);
+  const [zoom, setZoom] = useState(14);
+  const [clickCoordinate, setClickCoordinate] = useState(null);
+
+  useEffect(() => {
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        new VectorLayer({
+          source: new VectorSource(),
+        })
+      ],
+      view: new View({
+        center: fromLonLat(center),
+        zoom: zoom
+      })
+    });
+
+    const handleMapClick = (event) => {
+      const coordinate = event.coordinate;
+      setClickCoordinate(coordinate);
+    };
+
+    map.on('click', handleMapClick);
+
+    return () => {
+      map.setTarget(undefined);
+    };
+  }, [center, zoom]);
+
+  return (
+    <div>
+      <div ref={mapRef} style={{ width: '100%', height: '400px' }}></div>
+      {clickCoordinate && (
+        <div>
+          Clicked Coordinate: {clickCoordinate[0]}, {clickCoordinate[1]}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyMap;
