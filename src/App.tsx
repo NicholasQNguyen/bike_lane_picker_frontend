f4a94d16bf3832ec4d250652666ee7ea61a78042
Nicholas Nguyen <nicholasqnguyen8@gmail.com>
2025-02-24 07:17:35 -0500

    add error handling and a message if it failed

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,46 @@ import './App.css'
 import MyMap from "./components/MyMap/MyMap.tsx";
 import {useCoordinatesStore} from "./CoordinatesStore.tsx";
 import BluePillButton from "./components/BluePillButton/BluePillButton.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Description, Dialog, DialogPanel, DialogTitle} from "@headlessui/react";
-import {createClient} from "@supabase/supabase-js";
+import {createClient, SupabaseClient} from "@supabase/supabase-js";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const {coordinates} = useCoordinatesStore();
+  const FIVE_SECONDS = 5000;
 
-  const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseAnonKey = process.env.SUPABASE_KEY;
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  let supabase: SupabaseClient<any, 'public', any> | null = null;
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false)
+  const {coordinates} = useCoordinatesStore();
+  const [popupMessage, setPopupMessage] = useState("")
+
+  // Set up supabase credentials
+  useEffect(() => {
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseAnonKey = process.env.SUPABASE_KEY;
+    if (supabaseUrl === undefined) {
+      console.log("supabase url missing");
+      return;
+    }
+    if (supabaseAnonKey === undefined) {
+      console.log("supabase anon key missing");
+      return;
+    }
+    supabase = createClient(supabaseUrl, supabaseAnonKey);
+  }, []);
 
   // Function used to upload data to supabase db
   async function submitData() {
+    let confirmationMessage: string;
+    if (supabase === null) {
+      console.log("supabase connection not made! Did you set up your environment variables?");
+      confirmationMessage = "Error with database connection"
+      setIsConfirmationOpen(true)
+      setPopupMessage(confirmationMessage);
+      return;
+    }
+
     try {
       await supabase
         .from("longitude_and_latitude")
@@ -23,13 +49,23 @@ function App() {
           longitude: coordinates[0],
           latitude: coordinates[1]
         })
-      console.log("Submitted Data");
+      confirmationMessage = "Successfully sent data!";
     } catch (error) {
-      console.log("ERROR: ", error);
+      confirmationMessage = `Error: ${(error as Error).message}`;
     }
     // Close the popup
     setIsOpen(false);
+    // Show the success message
+    setIsConfirmationOpen(true)
+    setPopupMessage(confirmationMessage);
   }
+
+  // useEffect hook to close the success message popup
+  useEffect(() => {
+    setTimeout(() => {
+      setIsConfirmationOpen(false)
+    }, FIVE_SECONDS)
+  }, [isConfirmationOpen]);
 
   return (
     <>
@@ -53,6 +89,15 @@ function App() {
               <BluePillButton text={"Cancel"} onClick={() => setIsOpen(false)}/>
               <BluePillButton text={"Confirm"} onClick={submitData}/>
             </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+      <Dialog open={isConfirmationOpen} onClose={() => setIsConfirmationOpen(false)} className="relative z-50">
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+            <DialogTitle className="font-bold">Confirmation</DialogTitle>
+            <p>{popupMessage}</p>
           </DialogPanel>
         </div>
       </Dialog>
