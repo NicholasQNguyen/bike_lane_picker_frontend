cfab163b673b6160be3af4988a1f0e91ab9244e8
Nicholas Nguyen <nicholasqnguyen8@gmail.com>
2025-02-23 21:27:49 -0500

    cleanup on the map

--- src/components/MyMap/MyMap.tsx
+++ src/components/MyMap/MyMap.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -10,11 +10,9 @@ import 'ol/ol.css';
 import {useCoordinatesStore} from "../../CoordinatesStore.tsx";
 
 const MyMap = () => {
-  // ol.proj.transform([-8367174.179297738, 4859025.894138464],"WGS84", "EPSG:900913");
   const mapRef = useRef();
-  const [center, setCenter] = useState([-75.165, 39.952]);
-  const [zoom, setZoom] = useState(14);
-  const [clickCoordinate, setClickCoordinate] = useState([0, 0]);
+  const [center] = useState([-75.165, 39.952]);
+  const [zoom] = useState(14);
   const {updateCoordinates} = useCoordinatesStore();
 
   useEffect(() => {
@@ -37,7 +35,6 @@ const MyMap = () => {
     const handleMapClick = (event) => {
       const coordinate: number[] = event.coordinate;
       updateCoordinates(coordinate);
-      setClickCoordinate(coordinate);
     };
 
     map.on('click', handleMapClick);
@@ -50,11 +47,6 @@ const MyMap = () => {
   return (
     <div>
       <div ref={mapRef} style={{ width: '100%', height: '400px' }}></div>
-      {clickCoordinate && (
-        <div>
-          Clicked Coordinate: {clickCoordinate[0]}, {clickCoordinate[1]}
-        </div>
-      )}
     </div>
   );
 };
